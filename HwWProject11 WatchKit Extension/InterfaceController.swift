d44c177875e93060421c571348565d8c8b1e8169
Skyler Svendsen <skylersvendsen@gmail.com>
2017-12-28 21:09:48 -0700

    first commit

--- HwWProject11 WatchKit Extension/InterfaceController.swift
+++ HwWProject11 WatchKit Extension/InterfaceController.swift
@@ -8,19 +8,42 @@
 
 import WatchKit
 import Foundation
-
+import SpriteKit
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var gameInterface: WKInterfaceSKScene!
+    
+    var gameScene: GameScene!
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        startGame(self)
+    }
+    
+    @IBAction func startGame(_ sender: Any) {
+        if gameScene != nil {
+            guard gameScene.isPlayerAlive == false else { return }
+        }
+        
+        gameScene = GameScene(size: CGSize(width: 154, height: 174))
+        gameScene.parentInterfaceController = self
+        gameScene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        crownSequencer.focus()
+        crownSequencer.delegate = gameScene
+        
+        let transition = SKTransition.doorway(withDuration: 1)
+        gameInterface.presentScene(gameScene, transition: transition)
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        //crownSequencer.delegate = gameScene
     }
     
     override func didDeactivate() {
